f29ea52e69c1f1151718a8f2e8ef9bb5db7fa9c0
manishsharma2111 <manishsharmasad2111@gmail.com>
2019-07-12 14:22:37 +0530

    what the hell

---
+++ build/precache-manifest.21c7d315222231bc46272b064155b2e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e576fa39a88bf7dac0dd1e1fe3b3e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce47ebd8539d20d5a87",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bce47ebd8539d20d5a87",
+    "url": "/static/js/2.cd1156d8.chunk.js"
+  },
+  {
+    "revision": "236c5b06c720fa7c6e9d",
+    "url": "/static/js/main.747a722f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
