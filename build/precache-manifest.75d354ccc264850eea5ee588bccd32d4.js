732ee2b1768ff08520f310dc95a589e001934252
manishsharma2111 <manishsharmasad2111@gmail.com>
2019-07-12 12:25:10 +0530

    forth commit

---
+++ build/precache-manifest.75d354ccc264850eea5ee588bccd32d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c20a9e1a30c3df4a0ff2d45cd6a343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e39394671ef7ef1609",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a3e39394671ef7ef1609",
+    "url": "/static/js/2.4aa5c33b.chunk.js"
+  },
+  {
+    "revision": "ff504727cfb93d3b1652",
+    "url": "/static/js/main.2716aaf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
